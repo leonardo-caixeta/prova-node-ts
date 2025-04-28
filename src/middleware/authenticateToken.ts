3546facd3d265627a7e189ea2d002b1cdbf660f7
leonardo <leonardocaixeta07@gmail.com>
2025-04-28 17:34:10 -0300

    white commit

--- src/middleware/authenticateToken.ts
+++ src/middleware/authenticateToken.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export async function authenticateToken(
   req: Request,
